6a161f42884b88f825be4aaa92374c27a4794f25
Anant Sharma <anantsh.88@gmail.com>
2024-07-09 18:19:41 +0530

    added marshal formats for campaigns and ad_requests

--- controller/api/utils/marshal_formats.py
+++ controller/api/utils/marshal_formats.py
@@ -7,9 +7,31 @@ sponsor_output = {
     "budget": fields.Float,
 }
 
-influencers_output = {
+influencer_output = {
     "id": fields.Integer,
     "name": fields.String,
     "category": fields.String,
     "budget": fields.Float,
+}
+
+campaign_output = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "description": fields.String,
+    "start_date": fields.Date,
+    "end_date": fields.Date,
+    "budget": fields.Float,
+    "visibility": fields.Boolean,
+    "goals": fields.String,
+}
+
+ad_request_output = {
+    "id": fields.Integer,
+    "campaign_id": fields.Integer, 
+    "influencer_id": fields.Integer,
+    "messages": fields.String,
+    "requirements": fields.String,
+    "payment_amount": fields.Numeric,
+    "status": fields.Integer,
+        
 }
