4fd89417be9a18aeb7d91883d3d6c6a5a08aed40
Anant Sharma <anantsh.88@gmail.com>
2024-07-18 22:29:40 +0530

    Added Bootstrap-Flask and WTForms

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from model.db import db
-from controller.api.sponsors_api import SponsorsAPI
 from flask_bootstrap import Bootstrap5
+import secrets
+from flask_wtf.csrf import CSRFProtect
+
 
 def setup_app():
     app = Flask(__name__, template_folder="view")
     app.config.from_object(LocalDevelopmentConfig)
-    db.init_app(app)
-    api = Api(app)
-    CORS(app, resources={r"/*": {"origins": "*"}})
-    bootstrap = Bootstrap5(app)
-    return app, api, bootstrap
+    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lux'
+    app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+        api = Api(app)
+        CORS(app, resources={r"/*": {"origins": "*"}})
+        bootstrap = Bootstrap5(app)
+        csrf = CSRFProtect(app)
+        return app, api, bootstrap
 
 app, api, bootstrap = setup_app()
 
 with app.app_context():
-    db.create_all()
-
-api.add_resource(SponsorsAPI, "/api/sponsor/", "/api/sponsor/<sponsor_id>")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8099)
+    from controller.base import *
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=8099)
