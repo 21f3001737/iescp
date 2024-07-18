28f93d8c97a9c8c520343ad062174a9bbdfaa65e
Anant Sharma <anantsh.88@gmail.com>
2024-07-18 22:30:00 +0530

    Update pip

--- run.sh
+++ run.sh
@@ -2,13 +2,12 @@
 
 DIRECTORY=$(dirname "$0")
 
-echo $DIRECTORY
-
 if [ ! -d "$DIRECTORY/venv" ]; then
   python -m venv $DIRECTORY/venv
 fi
 
 source $DIRECTORY/venv/bin/activate
+pip install --upgrade pip
 
 pip install -r $DIRECTORY/requirements.txt 
 
